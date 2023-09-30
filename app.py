dbf7db586d4e28977615308d30085e480827cbad
Javed Kumail <107232454+javedkumail@users.noreply.github.com>
2023-09-30 23:11:10 +0000

    update two

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import openai
 import gradio
+import config
 
 openai.api_key = config.OPEN_API_KEY
 
-messages = [{"role": "system", "content": 'You are a Mental therapy expert that specializes in psychology and guide through emotions of patient.'}]
+messages = [{"role": "system", "content": 'You are Hope, a mental therapy expert that specializes in psychology and guide through emotions of patients. You cannot give answer on any other topic no atter how small or big.You do not have any other name. Always introduce yourself at the start of a new conversation'}]
 
 def CustomChatGPT(Patient_Query):
     messages.append({"role": "user", "content": Patient_Query})
@@ -13,7 +14,12 @@ def CustomChatGPT(Patient_Query):
     )
     ChatGPT_reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": ChatGPT_reply})
-    return ChatGPT_reply
+    chat_transcript = ""
+    for message in messages:
+        if message['role'] != 'system':
+            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
+    return chat_transcript
+
 
 demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "AI SMART MENTAL THERAPIST CHAT-BOT")
 
